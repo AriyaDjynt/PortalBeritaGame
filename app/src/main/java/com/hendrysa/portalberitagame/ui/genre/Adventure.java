5ba32532ada333b47c81dd17cc3179dd8312fcfc
hendrysa <46489744+hendrysa@users.noreply.github.com>
2020-01-08 12:01:08 +0700

    5_2

--- app/src/main/java/com/hendrysa/portalberitagame/ui/genre/Adventure.java
+++ app/src/main/java/com/hendrysa/portalberitagame/ui/genre/Adventure.java
@@ -1,4 +1,21 @@
 package com.hendrysa.portalberitagame.ui.genre;
 
-public class Adventure {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hendrysa.portalberitagame.R;
+
+public class Adventure extends AppCompatActivity {
+    String platform;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+
+        platform = getIntent().getStringExtra("platform");
+        Log.d("hendrysa", platform);
+    }
 }
--- app/src/main/java/com/hendrysa/portalberitagame/ui/genre/Simulation.java
+++ app/src/main/java/com/hendrysa/portalberitagame/ui/genre/Simulation.java
@@ -1,4 +1,21 @@
 package com.hendrysa.portalberitagame.ui.genre;
 
-public class Simulation {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hendrysa.portalberitagame.R;
+
+public class Simulation extends AppCompatActivity {
+    String platform;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+
+        platform = getIntent().getStringExtra("platform");
+        Log.d("hendrysa", platform);
+    }
 }
